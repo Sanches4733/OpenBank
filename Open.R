4b3ec4d74d0edd6ebd952a2bbc9b4542d7078ef2
Alexander Cherkasov <sanches4733@gmail.com>
2017-05-07 08:42:07 +0300

    Предварительное

--- Open.R
+++ Open.R
@@ -10,3 +10,7 @@ od <- od %>%
         mutate(Month = parse_number(Month)) %>%
         arrange(Bank,Month) %>%
         select (1,2,Requests = 4,Approval = 5,Issues = 3)
+
+od <- od %>%
+        mutate(Approved = Requests*Approval,Accepted = Issues/Approved) %>%
+        select(c(1,2,3,6,5,4,7))
